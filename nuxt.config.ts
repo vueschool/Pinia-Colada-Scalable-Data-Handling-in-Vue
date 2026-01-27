377129a376f03a3fe6efdd92d7f348403fae0b68
Daniel Kelly <me@danielkelly.io>
2026-01-27 09:51:17 -0600

    feat: install pinia colada

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   ssr: false,
   devtools: { enabled: true },
-  modules: ["@nuxt/hints", "@nuxt/icon", "@nuxt/image"],
+  modules: [
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@pinia/colada-nuxt",
+    "@pinia/nuxt",
+  ],
 });
